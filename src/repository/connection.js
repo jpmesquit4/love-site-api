c39b9c7d086903f7fdfff5d9dc37f3d25b554557
mesquita <joaopedromesq123@gmail.com>
2025-12-02 17:25:38 -0300

    OK

--- src/repository/connection.js
+++ src/repository/connection.js
@@ -4,8 +4,8 @@ let con = await mysql.createConnection({
   host: process.env.MYSQLHOST,
   port: process.env.MYSQLPORT || 3306,
   user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPWD,
-  database: process.env.MYSQL_DB,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQL_DATABASE,
   typeCast: function (field, next) {
       
     if (field.type === 'TINY' && field.length === 1) {
