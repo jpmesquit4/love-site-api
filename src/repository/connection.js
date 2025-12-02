152e64d30b6ff5db25edd6d4f523823d5f6a4da4
mesquita <joaopedromesq123@gmail.com>
2025-12-02 17:17:42 -0300

    alteração nas variaveis de ambiente

--- src/repository/connection.js
+++ src/repository/connection.js
@@ -2,6 +2,7 @@ import mysql from 'mysql2/promise'
 
 let con = await mysql.createConnection({
   host: process.env.MYSQL_HOST,
+  port: process.env.PORT || 3306,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PWD,
   database: process.env.MYSQL_DB,
