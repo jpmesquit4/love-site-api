532af799b5435109e170f83375dabbaf010e9aac
mesquita <joaopedromesq123@gmail.com>
2025-12-02 17:19:06 -0300

    OK

--- src/repository/connection.js
+++ src/repository/connection.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise'
 
 let con = await mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  port: process.env.PORT || 3306,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PWD,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPWD,
   database: process.env.MYSQL_DB,
   typeCast: function (field, next) {
       
